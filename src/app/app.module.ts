fff9a7f668576cc20607901472d61e4a12522b78
Kit Kogan <kkogan.dev@gmail.com>
2020-06-14 20:23:05 -0500

    establish header component connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+// import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+// import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+// import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
+// import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    // HeaderComponent,
+    // ShoppingListComponent,
+    // ShoppingListEditComponent,
+    // IngredientComponent,
+    // RecipeDetailComponent
   ],
   imports: [
     BrowserModule
